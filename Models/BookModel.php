45990435043406cb66d7d07228a19f70f533fff8
valdi1212 <valdi1212@gmail.com>
2016-03-01 10:02:28 +0000

    intial push

---
+++ Models/BookModel.php
@@ -0,0 +1,14 @@
+<?php
+
+class Model
+{
+    public $books;
+
+    public function __construct()
+    {
+        $this->books = [
+            ['name' => 'Game of Thrones', 'description' => 'Bók', 'author' => 'George R.R. Martin'],
+            ['name' => 'A Clash of Kings', 'description' => 'Önnur bók.', 'author' => 'George R.R. Martin']
+        ];
+    }
+}
